12a7aaee666c1baaf8ab576e31ce8c6dfe8571bc
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-07-16 02:22:56 -0500

    new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_072139) do
+ActiveRecord::Schema.define(version: 2019_07_16_072226) do
 
   create_table "instruments", force: :cascade do |t|
     t.string "brand"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_07_16_072139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
